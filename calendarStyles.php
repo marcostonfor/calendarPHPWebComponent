f06050225ea2773c6e3355000f6beb520f52fc19
marcos tonfor <marcostonfor@gmail.com>
2025-05-14 12:58:49 +0200

    Cambios mayoreas en los estilos del calendario. Posicionamiento de la navegacion abajo.

--- calendarStyles.php
+++ calendarStyles.php
@@ -17,6 +17,8 @@ function stylesCalendar($indexWeekDay, $actualDay, $evaluatedDay)
     if ((int) $actualDay === (int) $evaluatedDay) {
         $style .= ' background-color: dimgray; color: aliceblue; font-weight: bold;';
         $style .= ' text-shadow: 0vw 0vw 1px black;';
+    } elseif (((int) $actualDay === (int) $evaluatedDay) && ((int) $evaluatedDay === ((int) $indexWeekDay == 6))) {
+        $style .= ' background-color: dodgerblue; color: cornsilk;';
     }
 
     return $style;
@@ -34,9 +36,19 @@ function stylesCalendar($indexWeekDay, $actualDay, $evaluatedDay)
         opacity: 0.7;
         font-family: 'Frijole', sans-serif;
         empty-cells: hide;
+        border-top: 0.2vw double dimgray;
+        border-bottom: 0.2vw double dimgray;
+        border-top-right-radius: 0.5vw;
+        border-top-left-radius: 0.5vw;
+        
     }
 
-    td {
+    table th {
+        border-bottom: 0.2vw double black;
+        font-size: 15pt;
+    }
+
+    table td {
         border: .1vw double steelblue;
         border-collapse: separate;
         text-align: center;
@@ -44,9 +56,10 @@ function stylesCalendar($indexWeekDay, $actualDay, $evaluatedDay)
     }
 
     table th:nth-child(-n+5) {
-        color: dimgray;
-        text-shadow: -.1vw -.1vw 1px gray, .1vw .1vw 1px gray;
+        color: cornsilk;
         background-color: yellow;
+        opacity: 0.7; 
+        text-shadow: -.1vw -.1vw 1px black, .1vw .1vw 1px black;
     }
 
     table th:nth-child(n+6) {
@@ -73,6 +86,10 @@ function stylesCalendar($indexWeekDay, $actualDay, $evaluatedDay)
         background-color: lightpink;
     }
 
+    caption {
+        caption-side: bottom;
+    }
+
     caption .cal-nav {
         width: 100%;
         display: flex;
@@ -81,7 +98,7 @@ function stylesCalendar($indexWeekDay, $actualDay, $evaluatedDay)
         text-align: center;
         font-size: 1em;
         margin-bottom: 0.5em;
-        background-color: lightsteelblue;
+        background-color: goldenrod;
         color: darkslategray;
         padding: 0.3em 0;
     }
@@ -92,7 +109,6 @@ function stylesCalendar($indexWeekDay, $actualDay, $evaluatedDay)
         text-decoration: none;
         font-weight: bold;
         font-size: 25pt;
-        background-color: cadetblue;
     }
 
     caption .cal-nav strong {
